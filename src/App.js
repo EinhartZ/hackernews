24869f2748011ff348220003c74961739f0748ad
zhang_einhart <einhart.zhang@qaelum.com>
2019-10-17 11:32:42 +0200

    p38

--- src/App.js
+++ src/App.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'http://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'http://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+
+    const updatedList = this.state.list.filter(isNotId);
+
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    const helloWorld = 'Welcome to here ...';
+    return (
+      <div className="App">
+        <h2>{helloWorld}</h2>
+        {
+          this.state.list.map(item =>
+              <div key = {item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                  <button
+                    onClick = {() => this.onDismiss(item.objectID)}
+                    type="button"
+                  >
+                    Dismiss
+                  </button>
+                </span>
+              </div>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -22,5 +89,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
